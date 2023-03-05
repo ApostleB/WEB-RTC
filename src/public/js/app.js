eb470b66c4850233495ab61c46f88e3ab18a1747
ApostleB <jbu1391@gmail.com>
2023-03-06 02:13:32 +0900

    web-socket demo_final

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,50 @@
-console.log("hello");
+const messageList = document.querySelector ("ul");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nickname");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+//메세지 형식 맞추기
+const makeMessage = (type, payload) => {
+    const msg = {type, payload}
+    return JSON.stringify(msg);
+}
+
+const handleOpen = () => {
+    console.log("socket open");
+}
+socket.addEventListener('open', handleOpen )
+
+//메세지가 왔다!
+socket.addEventListener('message', (message) => {
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+    console.log("New Message : ", message.data); 
+})
+
+//서버와 통신이 끊겼다.
+socket.addEventListener('close', () => {
+    console.log("Disconnected Server");    
+})
+
+//메세지를 보내기
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const input = messageForm.querySelector('input');
+    socket.send(makeMessage("new_message", input.value));
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messageList.append(li);
+    input.value = "";
+}
+
+//닉네임에 대해서
+const handleNickSubmit = (e) => {
+    e.preventDefault();
+    const input = nickForm.querySelector('input');
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+}
+messageForm.addEventListener('submit', handleSubmit)
+nickForm.addEventListener("submit", handleNickSubmit)
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 const PORT = 3000;
 
@@ -10,5 +12,35 @@ app.get('/',(req, res) => {
     res.render('home');
 })
 
-const handleListen = () => console.log(`Listening on ${PORT}`);
-app.listen(PORT, handleListen);
+
+const handleListen = () => console.log(`Listening on http://localhost:${PORT}`);
+
+const server =http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const onSocketClose = (socket) => {
+    console.log("Disconnected from the Browser X");
+}
+
+const sockets = [];
+
+wss.on('connection', (socket) => {
+    sockets.push(socket);
+    socket['nickname'] = 'anon';
+    console.log("connection to Brower");
+    socket.on('close', onSocketClose )
+
+    socket.on('message', (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname" :
+                socket['nickname'] = message.payload;
+                console.log(message.payload);
+        }
+    })
+    
+});
+
+server.listen(PORT, handleListen);
